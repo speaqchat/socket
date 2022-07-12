558648a3b5817fb0e70bb01ee6e307096c07cd80
Francisco Silva <000franciscosilva000@gmail.com>
2022-07-12 21:36:33 +0100

    added webrtc methods

--- index.ts
+++ index.ts
@@ -50,6 +50,14 @@ io.on("connection", (socket: Socket) => {
     console.log(`${socket.id} disconnected!`);
     removeUser(socket.id);
   });
+
+  socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
 });
 
 httpServer.listen(process.env.PORT, () => {
